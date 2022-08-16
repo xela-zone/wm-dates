d5ac1d611888f434a8f2857310d793e57b45767e
Alexander Terry <alexterry43@gmail.com>
2022-08-15 17:17:35 -0800

    fix infinite loop on SW upgrade

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/wm-date/",
-  "pwa": {
-    "name": "Pickable Dates",
-    "themeColor": "#00bcd4",
-    "shortName": "Pickable Dates",
-    "display": "standalone",
-    "start_url": "/wm-date/",
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    },
+    name: "Pickable Dates",
+    themeColor: "#00bcd4",
+    shortName: "Pickable Dates",
+    display: "standalone",
+    start_url: "/wm-date/",
   }
 })
