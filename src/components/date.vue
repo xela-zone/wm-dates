5513a2897de2f494c3068eea845a0ad4ee311f8b
Alexander Terry <alexterry43@gmail.com>
2022-08-28 21:31:05 -0800

    better date stuff, getMonth returns 0 index

--- src/components/date.vue
+++ src/components/date.vue
@@ -15,15 +15,21 @@
 <script>
 export default {
   name: "DateComponent",
+  data() {
+    return {date: new Date()}
+  },
+mounted() {
+    self.setInterval(() => {this.date = new Date()}, 1000*60)
+
+  },
   props: {
     msg: String,
     days: Number,
   },
   computed: {
     expiration() {
-      var result = new Date();
-      result.setDate(result.getDate() + this.days);
-      return `${result.getMonth()}/${result.getDate()}/${result.getFullYear()}`;
+      this.date.setDate(this.date.getDate() + this.days);
+      return `${this.date.getMonth() + 1 }/${this.date.getDate()}/${this.date.getFullYear()}`;
     },
   },
 };
