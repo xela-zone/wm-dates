fbdaebe4e3f9dd6dde4bc0db0b00f2fd9cd41656
Alexander Terry <alexterry43@gmail.com>
2022-09-12 20:21:45 -0800

    add month abbriviation

--- src/components/date.vue
+++ src/components/date.vue
@@ -28,8 +28,10 @@ mounted() {
   },
   computed: {
     expiration() {
+      //  list of abriviations for months
+      let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
       this.date.setDate(this.date.getDate() + this.days);
-      return `${this.date.getMonth() + 1 }/${this.date.getDate()}/${this.date.getFullYear()}`;
+      return `(${months[this.date.getMonth()]}) ${this.date.getMonth() + 1 }/${this.date.getDate()}/${this.date.getFullYear()}`;
     },
   },
 };
