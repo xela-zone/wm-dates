019e5e66f129a3263bb8551f985b3719a32ade02
Alexander Terry <alexterry43@gmail.com>
2024-05-07 15:47:45 -0800

    move search to above table

--- src/components/searcher.vue
+++ src/components/searcher.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <input type="search" :value="search" @input="e => search = e.target.value" placeholder="Search for a PLU Here...">
+        <!--  :value and @input from https://github.com/vuejs/vue/issues/8231#issuecomment-547391171 -->
         <table v-if="FilteredPlus.length < 20">
             <thead>
                 <td>Thing</td>
@@ -18,8 +20,6 @@
                 </tr>
             </tbody>
         </table>
-        <input type="search" :value="search" @input="e => search = e.target.value" placeholder="Search for a PLU Here...">
-        <!--  :value and @input from https://github.com/vuejs/vue/issues/8231#issuecomment-547391171 -->
     </div>
 
 </template>
